006016d7f9efdde2ad0413c4185ed7b40e935507
= <=>
2024-02-22 16:56:52 -0500

    feat: Add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Store Api')
